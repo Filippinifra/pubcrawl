c4ba707782cdfd34ccad85e10ef71ed0c9ef2349
filippinifra <filippinifra123@gmail.com>
2021-09-25 15:25:14 +0200

    Add labels for laurea Gaspe

--- src/constants/labels.js
+++ src/constants/labels.js
@@ -1,9 +1,8 @@
-export const TITLE = "PUB CRAWL";
-export const START_DATE = "Venerdì 25 SETTEMBRE";
+export const TITLE = "FESTA LAUREA GASPE";
+export const START_DATE = "Venerdì 8 Ottobre";
 export const LOCATION_PLACE = "LAMBRATE";
-export const START_TIME = "21.00";
-export const TIP =
-  "A ogni tappa verrà imposta una regola diversa che tutti dovranno rispettare. Le pub rules verranno rivelate all'inizio di ogni tappa.";
+export const START_TIME = "21.30";
+export const TIP = "A ogni tappa verrà imposta una regola diversa che tutti dovranno rispettare. Le pub rules verranno rivelate all'inizio di ogni tappa.";
 
 export const LOCATION = "Location";
 export const DATE = "Data";
